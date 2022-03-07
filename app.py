58748650fe6a95ae94a25cdba518ac5dc1d901b8
amoshochman <amoshochman1982@gmail.com>
2022-03-07 16:45:55 +0200

    massive changes again

--- app.py
+++ app.py
@@ -10,49 +10,52 @@ app = Flask(__name__)
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     task_title = db.Column(db.String(100), unique=False, nullable=False)
-    task_notes = db.Column(db.String(200), unique=False, nullable=True)
     task_is_done = db.Column(db.Integer, unique=False, nullable=False)
 
     def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns if getattr(self, c.name)}
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns if getattr(self, c.name) is not None}
 
-    def __init__(self, task_title, notes=None):
+    def __init__(self, task_title):
         self.task_title = task_title
         self.task_is_done = 0
-        if notes:
-            self.task_notes = notes
 
 
-@app.route('/tasks', methods=['GET', 'DELETE'])
-def existing_tasks():
+@app.route('/tasks', methods=['GET', 'DELETE', 'POST'])
+def tasks_id_not_provided():
     if request.method == 'GET':
         return jsonify([task.as_dict() for task in Task.query.all()])
+    elif request.method == 'DELETE':
+        tasks = Task.query.all()
+        for task in tasks:
+            db.session.delete(task)
+        db.session.commit()
+        return "all items were removed"
     else:
-        return "hello"
-        # tasks = Task.query.all()
-        # db.session.delete(tasks)
+        task_title = request.form.get('task_title')
+        task = Task(task_title)
+        db.session.add(task)
+        db.session.commit()
+        return jsonify(task.as_dict())
 
 
 @app.route('/tasks/<task_id>', methods=['GET', 'DELETE'])
-def existing_task(task_id):
+def tasks_id_yes_provided(task_id):
     if request.method == 'DELETE':
         task = Task.query.filter_by(task_id=task_id).first_or_404()
         db.session.delete(task)
         db.session.commit()
-        return "ok"
+        return jsonify(task.as_dict())
     else:
         task = Task.query.filter_by(task_id=task_id).first_or_404()
         return jsonify(task.as_dict())
 
 
-@app.route('/tasks', methods=['POST'])
-def new_task():
-    task_title = request.form.get('task_title')
-    notes = request.form.get('notes')
-    task = Task(task_title)
-    db.session.add(task)
+@app.route('/tasks/done/<task_id>', methods=['PUT'])
+def mark_task_done(task_id):
+    task = Task.query.filter_by(task_id=task_id).first_or_404()
+    task.task_is_done = 1
     db.session.commit()
-    return {"task_id": task.task_id}
+    return jsonify(task.as_dict())
 
 
 def create_app(is_test=False):
--- test_tasks.py
+++ test_tasks.py
@@ -1,34 +1,44 @@
 TASK_TITLE = "buying a glass"
 
 
-def post(client):
-    data = {"task_title": TASK_TITLE}
-    return client.post("/tasks", data=data)
+def create_new_task(client, task_title=TASK_TITLE):
+    data = {"task_title": task_title}
+    response = client.post("/tasks", data=data)
+    return response.json['task_id']
 
 
 def test_post_get(client):
-    response = post(client)
-    assert response.status_code == 200
+    create_new_task(client)
     response = client.get("/tasks")
     rows = response.json
     assert [elem for elem in rows if elem['task_title'] == TASK_TITLE]
 
 
-def test_post_delete(client):
-    response = post(client)
-    assert response.status_code == 200
+def test_post_delete_all(client):
+    create_new_task(client)
+    response = client.get("/tasks")
+    assert response.json
     response = client.delete("/tasks")
     assert response.status_code == 200
     response = client.get("/tasks")
-    assert response.status_code == 200
+    assert not response.json
 
 
-def test_delete_specific(client):
-    response = post(client)
-    task_id = response.json['task_id']
+def test_post_delete_specific(client):
+    task_id = create_new_task(client)
     response = client.get("/tasks/" + str(task_id))
     assert response.json
     response = client.delete("/tasks/" + str(task_id))
     assert response.status_code == 200
     response = client.get("/tasks/" + str(task_id))
     assert not response.json
+
+
+def test_mark_task_done(client):
+    task_id = create_new_task(client)
+    response = client.get("/tasks/" + str(task_id))
+    assert response.json['task_is_done'] == 0
+    response = client.put("/tasks/done/" + str(task_id))
+    assert response.status_code == 200
+    response = client.get("/tasks/" + str(task_id))
+    assert response.json['task_is_done'] == 1
